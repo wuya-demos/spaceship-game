e4e50204219f43e51e0a8d16a6a1f0a1c6e79c30
freewind <nowindlee@gmail.com>
2018-08-11 21:30:38 +0800

    can move the spaceship by arrow keys now

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const canvas = document.getElementById("my-canvas");
 const ctx = canvas.getContext("2d");
 ctx.beginPath();
 
-function drawSpaceship(x, y) {
+let shipX = 400, shipY = 400;
+
+function drawSpaceship() {
     ctx.fillStyle = "#FF0000";
     const size = 50;
-    ctx.fillRect(x, y, size, size);
+    ctx.fillRect(shipX, shipY, size, size);
 }
 
 function drawRock(x, y) {
@@ -20,9 +22,67 @@ function drawBullet(x, y) {
     ctx.fillRect(x, y, size, size);
 }
 
-drawSpaceship(400, 400);
-drawBullet(140, 100);
-drawBullet(400, 200);
-drawRock(200, 200);
-drawRock(300, 500);
-drawRock(600, 100);
+
+function drawAll() {
+    drawRock(200, 200);
+    drawRock(300, 500);
+    drawRock(600, 100);
+
+    drawBullet(140, 100);
+    drawBullet(400, 200);
+
+    drawSpaceship();
+}
+
+drawAll();
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function redrawAll() {
+    clearCanvas();
+    drawAll();
+}
+
+function moveLeft() {
+    shipX -= 3;
+    redrawAll();
+}
+
+function moveRight() {
+    shipX += 3;
+    redrawAll();
+}
+
+function moveUp() {
+    shipY -= 3;
+    redrawAll();
+}
+
+function moveDown() {
+    shipY += 3;
+    redrawAll();
+}
+
+document.addEventListener("keydown", function (event) {
+    const LEFT = 37;
+    const RIGHT = 39;
+    const UP = 38;
+    const DOWN = 40;
+
+    switch (event.keyCode) {
+        case LEFT:
+            moveLeft();
+            break;
+        case RIGHT:
+            moveRight();
+            break;
+        case UP:
+            moveUp();
+            break;
+        case DOWN:
+            moveDown();
+            break;
+    }
+});
